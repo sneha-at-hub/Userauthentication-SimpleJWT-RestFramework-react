440eeb5ba73b515993f08528ba3e8ded221164b6
sneha-at-hub <snehatmg2004@gmail.com>
2024-07-15 12:10:33 +0545

    Created Serializer

---
+++ backend/api/urls.py
@@ -0,0 +1,5 @@
+from rest_framework_simplejwt.tokens import Token
+
+from django.urls import path
+
+from . import views
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=50),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
